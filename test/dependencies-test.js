c6e6734fa2a76458a97852a048edb318242e61b1
Jonas Enge <jonas.enge@gmail.com>
2018-09-03 14:22:32 +0200

    Div modernize fixes

--- test/dependencies-test.js
+++ test/dependencies-test.js
@@ -1,19 +1,21 @@
 const test = require('ava')
-const pkg = require('../../package.json')
+const pkg = require('../package.json')
 const { dependencies, devDependencies } = pkg
+const dropModules = ['micro-dev']
+const isDropped = module => !dropModules.includes(module)
 
 test('basic check', t => {
   t.true(true, 'ava works ok')
 })
 
-Object.keys(dependencies).forEach(dependency => {
+Object.keys(dependencies).filter(isDropped).forEach(dependency => {
   test(`${dependency} loads ok`, t => {
     const module = require(dependency)
     t.truthy(module)
   })
 })
 
-Object.keys(devDependencies).forEach(dependency => {
+Object.keys(devDependencies).filter(isDropped).forEach(dependency => {
   test(`${dependency} loads ok`, t => {
     const module = require(dependency)
     t.truthy(module)
