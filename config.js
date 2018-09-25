fe63c14529c63bea6bd2398c21c2d22f8039c01e
Jonas Enge <jonas.enge@gmail.com>
2018-09-25 14:07:03 +0200

    Supports switch to masseoppslag

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ module.exports = {
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345,
   DSF_CONFIG: {
     url: process.env.DSF_URL || 'http://ws-test.infotorg.no/xml/ErgoGroup/DetSentraleFolkeregister1_4/2015-08-10/DetSentraleFolkeregister1_4.wsdl',
+    urlMasseOppslag: process.env.DSF_URL_MASSEOPPSLAG || 'http://ws.infotorg.no/xml/ErgoGroup/DSFMasseoppslag1_4/2015-08-10/DSFMasseoppslag1_4.wsdl',
     namespaceBrukersesjon: process.env.DSF_NAMESPACE || 'http://ws.infotorg.no/xml/Admin/Brukersesjon/2006-07-07/Brukersesjon.xsd',
     distribusjonskanal: process.env.DSF_DIST || 'PTP',
     systemnavn: process.env.DSF_SYSTEM_NAVN || 'systemnavn',
